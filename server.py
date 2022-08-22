d51e578cee7d9d39b25b9fdcddc0044ea7f53dc3
Zwel Nyi Nyar Htut <zwelnyinyarhtut@Macintosh.local>
2022-08-22 15:18:28 +0630

    got template working

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
+import json
 
+w = json.load(open("worldl.json"))
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "riki was here"
-    
+    return render_template("index.html")
+
 app.run(debug=True)
