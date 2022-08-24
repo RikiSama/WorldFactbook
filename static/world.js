481a0b3ec4bf3d4c446d83303c079fd6e60de14c
Zwel Nyi Nyar Htut <zwelnyinyarhtut@Macintosh.local>
2022-08-25 01:17:55 +0630

    create country delete button

--- static/world.js
+++ static/world.js
@@ -15,16 +15,31 @@ document.body.onload= async ()=>{
                 cdiv.onclick = async() => {
                     let ret = await fetch(`/api/country/${c}`);
                     let countryData = await ret.json();
-                    document.getElementById('countryDetail').innerHTML = `
-                    <div> Name: ${countryData.result[0].name}</div>
-                    <div> Continent: ${countryData.result[0].continent}</div>
-                    <img src='${countryData.result[0].flag}'>
-                    `;
+                    showOneCountry(countryData.result[0]);
                 }
                 document.getElementById('listOfCountries').append(cdiv);
             }
         }
+        let contList = document.getElementById('continentList');
+        if (contList){
         document.getElementById('continentList').append(div,' ');
-        
+        }
     }
+}
+
+function showOneCountry(cobj){
+    document.getElementById('countryDetail').innerHTML = `
+        <div> Name: ${cobj.name}</div>
+        <div> Continent: ${cobj.continent}</div>
+        <div> Capial: ${cobj.capital}</div>
+        <div> Area: ${cobj.area}</div>
+        <div> Population: ${cobj.population}</div>
+        <div> Gross Domestic Product: ${cobj.gdp}</div>
+        <img src='${cobj.flag}'>
+        <button onclick='doDelete()'>Delete</delete>
+        `;
+}
+
+function doDelete(){
+    console.log("You tried to delete");
 }
