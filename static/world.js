016155594b272350ec7cc7da2f11aa5813058944
Zwel Nyi Nyar Htut <zwelnyinyarhtut@Macintosh.local>
2022-08-25 23:36:44 +0630

    assign type number in area,population,dgp and dorp down the continent in create new country

--- static/world.js
+++ static/world.js
@@ -1,15 +1,52 @@
 document.getElementById('newCountry').onclick = async () => {
     document.getElementById('countryDetails').innerHTML='';
+
     let divName = document.createElement('div');
     divName.innerHTML = `<label>Country Name: <input id=CountryName></label>`;
 
+    let divCapital = document.createElement('div');
+    divCapital.innerHTML = `<label>Capital: <input id=Capital></label>`;
+
     let divID = document.createElement('div');
-    divID.innerHTML = `<label>ID: <input id=ID></label>`;
+    divID.innerHTML = `<label>ID: <input id=CID></label>`;
+
+    let divArea = document.createElement('div');
+    divArea.innerHTML = `<label>Area: <input type="number" id=Area></label>`;
+
+    let divPopulation = document.createElement('div');
+    divPopulation.innerHTML = `<label>Population: <input type="number" id=Population></label>`;
+
+    let divContinent = document.createElement('div');
+    divContinent.innerHTML = `<label>Continent: <select id="Continent"> <option>Africa</option> <option>Asia</option> <option>Caribbean</option> <option>Eurasia</option> <option>Europe</option> <option>North America</option> <option>Oceania</option> <option>South America</option> </select></label>`;
+
+    let divGDP = document.createElement('div');
+    divGDP.innerHTML = `<label>Gross Domestic Product: type="number" <input id=GDP></label>`;
 
     let addbotton = document.createElement('button');
     addbotton.innerText = 'Add New Country'
+
+    addbotton.onclick = () =>{
+        let payload={
+            
+                "result": [
+                  {
+                    "area": parseInt(document.getElementById('Area').value),
+                    "capital": document.getElementById('Capital').value,
+                    "continent": document.getElementById('Continent').vale,
+                    "flag": "//upload.wikimedia.org/wikipedia/commons/2/2f/Flag_of_Armenia.svg",
+                    "gdp": parseInt(document.getElementById('GDP').value),
+                    "id": parseInt(document.getElementById('CID').value),
+                    "name": document.getElementById('CountryName').value,
+                    "population": parseInt(document.getElementById('Population').value),
+                    "tld": ".am"
+                  }
+                ]
+              };
+              fetch('/api/newCountry',{method:'PUT',body: JSON.stringify(payload)});
+
+        }
     
-    document.getElementById('countryDetails').append(divName,divID,addbotton);
+    document.getElementById('countryDetails').append(divName,divID,divContinent,divCapital,divArea,divPopulation,divGDP,addbotton);
 }
 document.body.onload= async ()=>{
     let ret =await fetch("/api/continentList");
